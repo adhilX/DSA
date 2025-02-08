e8dabfc2f1338581000cbb7e084380773b1260d3
adhil2399 <adhi23996@gmail.com>
2025-02-08 14:44:56 +0530

    bubble sort

---
+++ Week2/bubbleSort.js
@@ -0,0 +1,16 @@
+
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] < arr[j - 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      }
+    }
+  }
+  return arr
+}
+
+console.log(bubbleSort([3, 4, 5, 2, 33, 45, 8, 5, 4, 87]));
